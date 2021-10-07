b9cb83abd0a23ad6cee69b517b8375a5e4371c6b
Yakumo Saki <yakumo@ziomatrix.org>
2021-10-07 13:38:39 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# syntax=docker/dockerfile:1.3-labs
+FROM python:3.6
+RUN apt-get update && \
+apt-get install -y git && \
+pip install Pillow chameleon nml==0.5.3 markdown
+
+# FIRS (XIS)
+# nml > 0.6.0 causes error. need 0.5.3
+# nmlc ERROR: "generated/firs.nml", line 7394: Temporary registers from 128 to 255 are reserved for NML's internal calculations.
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENV GIT_REPO=NONE
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -eu
+
+if [ "$GIT_REPO" = "NONE" ]; then
+    echo "Git repository not specified."
+    echo "Method 1) Use this image using automated build. (make)"
+    echo "docker run -e GIT_REPO=https://example.com/.git -v result.grf:/result.grf yakumosaki/openttd-newgrf-builder"
+    echo "Built NewGRF is 'result.grf' on your current directory"
+    echo "-"
+    echo "Method 2) Use this image using manual build."
+    echo "docker run -it --entrypoint bash yakumosaki/openttd-newgrf-builder"
+    echo "    --- and manualy build ---"
+    echo "docker cp containerid:filepath hostpath"
+    echo ""
+    exit 127
+fi
+
+echo "====================================================="
+echo "Clone repo"
+echo "====================================================="
+git clone $GIT_REPO newgrf
+cd newgrf
+
+echo "====================================================="
+echo "Build"
+echo "====================================================="
+make
+
+echo "====================================================="
+echo "Copy artifact"
+echo "====================================================="
+echo "Find *.grf"
+find . -type f -name *.grf
+
+NEWGRF=`find . -type f -name *.grf | head -n 1`
+echo "artifact is $NEWGRF. copying to /result.grf"
+cp $NEWGRF /result.grf
+
+echo "*****************************************************"
+echo "                      Done"
+echo "*****************************************************"
